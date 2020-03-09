15176c02ab1a5435fbc7fa0c49ef91edc3a0a39a
JuanmaMartin14 <61692322+JuanmaMartin14@users.noreply.github.com>
2020-03-09 08:56:09 -0300

    Add files via upload

---
+++ roman_numbers.py
@@ -0,0 +1,21 @@
+def roman_to_decimal(roman_numbers):
+	if roman_numbers=='I':
+		return(1)
+	if roman_numbers=='II':
+		return(2)
+	if roman_numbers=='III':
+		return(3)
+	if roman_numbers=='IV':
+		return(4)
+	if roman_numbers=='V':
+		return(5)
+	if roman_numbers=='VI':
+		return(6)
+	if roman_numbers=='VII':
+		return(7)
+	if roman_numbers=='VIII':
+		return(8)
+	if roman_numbers=='IX':
+		return(9)
+	if roman_numbers=='X':
+		return(10)
